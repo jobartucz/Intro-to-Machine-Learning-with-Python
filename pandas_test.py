b4e40b3c3ee20448f0864eb9066ba09c982d97ed
John Bartucz <jbartucz@Johns-Air.roc.mn.charter.com>
2020-04-20 22:47:53 -0500

    it works!

--- pandas_test.py
+++ pandas_test.py
@@ -1,6 +1,15 @@
+import time
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+plt.close('all')
 
-df = pd.DataFrame(np.random.randn(1000, 4), columns=['A','B','C','D'])
-print(df)
-pd.plotting.scatter_matrix(df, alpha=0.2)
+if False:
+    ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+    ts = ts.cumsum()
+    ts.plot()
+else:
+    df = pd.DataFrame(np.random.randn(1000, 4), columns=['A','B','C','D'])
+    pd.plotting.scatter_matrix(df, alpha=0.2)
+
+plt.show()
